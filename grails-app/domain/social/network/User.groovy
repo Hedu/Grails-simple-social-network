7de09037efd09b4e5fbe3a7be6dcf6d7e08a94f8
hedu <eperezghedu@gmail.com>
2016-11-13 00:45:41 +0100

    resolves #3 define user attributes

--- grails-app/domain/social/network/User.groovy
+++ grails-app/domain/social/network/User.groovy
@@ -2,6 +2,20 @@ package social.network
 
 class User {
 
+    String firstName
+    String lastName
+    String email
+    String passwordHash
+    String gender
+    Date birthDay
+    static hasMany = [friends: User]
+
     static constraints = {
+      firstName size: 3..75, blank: false, nullable: false
+      lastName size: 3..75, blank: false, nullable: false
+      email email: true, unique: true, blank: false, nullable: false
+      passwordHash password: true, size: 8..20, blank: false, nullable: false
+      gender inList: ["Male", "Female"]
+      friends display: false
     }
 }
